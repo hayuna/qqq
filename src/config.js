69141e7eac56fa9a60241167c331684fa373a69f
kowalonm <marek.kowalonek@contractors.roche.com>
2021-06-04 07:22:20 +0200

    EPICXCORE-8274 - Add parentApiKey for config file

--- src/config.js
+++ src/config.js
@@ -1,14 +1,38 @@
 export default {
     SANDBOX: {
-        partnerId: '2287542'
+        partnerId: '2287542',
+        parentApiKey: {
+            EU: '3_nCwS3TE9I5eMlk9RI_FMqzCbmbd5SW_a2tvF1_d9g-SNYnc5g16bTwZVHvd515vq', 
+            US: '3_cUCYzkV3NsKYwkZoHPo9pv1SkPGnm0C2zYS2ErFAXq-fmXsOfqvIn9PVnrFOlh-c', 
+            CH: '', 
+            RU: ''
+        }
     },
     DEV: {
-        partnerId: ''
+        partnerId: '',
+        parentApiKey: {
+            EU: '', 
+            US: '', 
+            CH: '', 
+            RU: ''
+        }
     },
     TEST: {
-        partnerId: ''
+        partnerId: '',
+        parentApiKey: {
+            EU: '', 
+            US: '', 
+            CH: '', 
+            RU: ''
+        }
     },
     PROD: {
-        partnerId: ''
+        partnerId: '',
+        parentApiKey: {
+            EU: '', 
+            US: '', 
+            CH: '', 
+            RU: ''
+        }
     },
 }
